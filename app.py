97b8bf06a6bb074dba34422c922d6003f20be0b6
RTANC <34498692+RTANC@users.noreply.github.com>
2022-10-31 16:30:31 +0700

    add http api service

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, jsonify
+from flask_restful import Api, Resource
+from werkzeug.utils import secure_filename
+from detect import detector
+import os
+from PIL import Image
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+app = Flask(__name__)
+api = Api(app)
+
+class malaria(Resource):
+    def post(self):
+        if 'file' not in request.files:
+            resp = jsonify({'message' : 'No file part in the request'})
+            resp.status_code = 400
+            return resp
+        file = request.files['file']
+        if file.filename == '':
+            resp = jsonify({'message' : 'No file selected for uploading'})
+            resp.status_code = 400
+            return resp
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            input_image = Image.open(file)
+            # img.save(os.path.join('output', filename))
+            boxes = detector(input_image)
+            resp = jsonify(boxes)
+            resp.status_code = 201
+            return resp
+        else:
+            resp = jsonify({'message' : 'Allowed file types are png, jpg, jpeg'})
+            resp.status_code = 400
+            return resp
+
+api.add_resource(malaria, "/malaria")
+
+if __name__ == "__main__":
+    app.run(debug=True)
